c57619ddb1a134a2782471b272d20c58768f2079
Isac Maximo <88113372+isacmaximo@users.noreply.github.com>
2025-09-27 15:49:25 -0300

    Executando Bloc Com Package

--- lib/app/data/blocs/tarefa_bloc.dart
+++ lib/app/data/blocs/tarefa_bloc.dart
@@ -1,32 +1,24 @@
 import 'dart:async';
 
+import 'package:bloc/bloc.dart';
 import 'package:bloc_puro/app/data/blocs/tarefa_event.dart';
 import 'package:bloc_puro/app/data/blocs/tarefa_state.dart';
 import 'package:bloc_puro/app/data/models/tarefa_model.dart';
 import 'package:bloc_puro/app/data/repositories/tarefa_repository.dart';
 
-class TarefaBloc {
+//Bloc<Entrada,Saida>
+class TarefaBloc extends Bloc<TarefaEvent, TarefaState> {
   final _repository = TarefaRepository();
 
-  final StreamController<TarefaEvent> _inputTarefaController =
-      StreamController<TarefaEvent>();
-  final StreamController<TarefaState> _outputTarefaController =
-      StreamController<TarefaState>();
-
-  //expondo a entrada para o view
-  Sink<TarefaEvent> get inputTarefa => _inputTarefaController.sink;
-  //expondo a saída
-  Stream<TarefaState> get outputTarefa => _outputTarefaController.stream;
-
-  TarefaBloc() {
-    //fica ouvindo todos os eventos
-    _inputTarefaController.stream.listen(_mapEventToState);
+  //passado o estado inicial via super
+  TarefaBloc() : super(TarefaInitalState()) {
+    on(_mapEventToState);
   }
 
-  void _mapEventToState(TarefaEvent event) async {
+  void _mapEventToState(TarefaEvent event, Emitter emit) async {
     List<TarefaModel> tarefas = [];
 
-    _outputTarefaController.add(TarefaLoadingState());
+    emit(TarefaLoadingState());
 
     if (event is GetTarefas) {
       tarefas = await _repository.getTarefas();
@@ -36,6 +28,6 @@ class TarefaBloc {
       tarefas = await _repository.deleteTarefa(tarefa: event.tarefa);
     }
 
-    _outputTarefaController.add(TarefaLoadedState(tarefas: tarefas));
+    emit(TarefaLoadedState(tarefas: tarefas));
   }
 }
--- lib/app/pages/tarefas_page.dart
+++ lib/app/pages/tarefas_page.dart
@@ -3,6 +3,7 @@ import 'package:bloc_puro/app/data/blocs/tarefa_event.dart';
 import 'package:bloc_puro/app/data/blocs/tarefa_state.dart';
 import 'package:bloc_puro/app/data/models/tarefa_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TarefasPage extends StatefulWidget {
   const TarefasPage({super.key});
@@ -18,20 +19,20 @@ class _TarefasPageState extends State<TarefasPage> {
   void initState() {
     super.initState();
     _tarefaBloc = TarefaBloc();
-    _tarefaBloc.inputTarefa.add(GetTarefas());
+    _tarefaBloc.add(GetTarefas());
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Bloc Pattern')),
-      body: StreamBuilder<TarefaState>(
-        stream: _tarefaBloc.outputTarefa,
+      body: BlocBuilder<TarefaBloc, TarefaState>(
+        bloc: _tarefaBloc,
         builder: (context, state) {
-          if (state.data is TarefaLoadingState) {
+          if (state is TarefaLoadingState) {
             return Center(child: CircularProgressIndicator());
-          } else if (state.data is TarefaLoadedState) {
-            final list = state.data?.tarefas ?? [];
+          } else if (state is TarefaLoadedState) {
+            final list = state.tarefas;
             return ListView.separated(
               itemCount: list.length,
               itemBuilder: (context, index) {
@@ -42,9 +43,7 @@ class _TarefasPageState extends State<TarefasPage> {
                   title: Text(list[index].nome),
                   trailing: IconButton(
                     onPressed: () {
-                      _tarefaBloc.inputTarefa.add(
-                        DeleteTarefa(tarefa: list[index]),
-                      );
+                      _tarefaBloc.add(DeleteTarefa(tarefa: list[index]));
                     },
                     icon: Icon(Icons.delete_rounded, color: Colors.red),
                   ),
@@ -62,7 +61,7 @@ class _TarefasPageState extends State<TarefasPage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _tarefaBloc.inputTarefa.add(
+          _tarefaBloc.add(
             PostTarefa(tarefa: TarefaModel(nome: 'Fazer Caminhada')),
           );
         },
@@ -73,7 +72,7 @@ class _TarefasPageState extends State<TarefasPage> {
 
   @override
   void dispose() {
-    _tarefaBloc.inputTarefa.close();
+    _tarefaBloc.close();
     super.dispose();
   }
 }
